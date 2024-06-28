e315d4a080a27410fb81ce1ec8f875bed64c1669
florien2208 <mahorompakanyiflorien@gmail.com>
2024-06-28 12:51:41 +0200

    formatted string

--- learn.py
+++ learn.py
@@ -1,5 +1,13 @@
-print("this dj flo Learning python")
-f = input("what is your name? ")
-s = input("what is your favorite color ")
+# print("this dj flo Learning python")
+# f = input("what is your name? ")
+# s = input("what is your favorite color ")
+#
+# print("hi"+f + "and "+ s)
 
-print("hi"+f + "and "+ s)
+#
+# a = input('enter your age? ')
+# b = 2024 - int(a)
+# print('your age is ', b)
+a = 'hello'
+b='flo'
+print(f'{a} dj {b}')
