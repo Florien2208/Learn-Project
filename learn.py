c58a38197f137d886374094935eb8be8ad2493aa
florien2208 <mahorompakanyiflorien@gmail.com>
2024-06-29 08:45:35 +0200

    updates

--- learn.py
+++ learn.py
@@ -1,13 +1,21 @@
-# print("this dj flo Learning python")
-# f = input("what is your name? ")
-# s = input("what is your favorite color ")
-#
-# print("hi"+f + "and "+ s)
+print("this dj flo Learning python")
+f = input("what is your name? ")
+s = input("what is your favorite color ")
 
-#
-# a = input('enter your age? ')
-# b = 2024 - int(a)
-# print('your age is ', b)
+print("hi"+f + "and "+ s)
+
+
+a = input('enter your age? ')
+b = 2024 - int(a)
+print('your age is ', b)
 a = 'hello'
-b='flo'
+b = 'flo'
 print(f'{a} dj {b}')
+print(a.upper())
+print(a.find('l'))
+print(a.replace('e','oe'))
+print('el' in a)
+print(20 // 3)
+print(20 ** 4)
+import math
+print(math.floor(6.8))
